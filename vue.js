92aef6d94948f1d4fa4586bd305816bc1ee05982
Khalil <khalil.abudahab@cgps.group>
2022-07-06 10:08:44 +0100

    Create vue.js

---
+++ vue.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    "plugin:vue/strongly-recommended",
+  ],
+  "rules": {
+    "vue/attributes-order": [ "error" ],
+    "vue/order-in-components": [ "error" ],
+    "vue/require-default-prop": 0,
+    "vue/v-bind-style": [ "error", "longform" ],
+    "vue/v-on-style": [ "error", "longform" ],
+  },
+};
