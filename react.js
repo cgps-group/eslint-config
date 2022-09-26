9bd82aa416ab30629d341b9679966232ff49a882
Khalil <khalil.abudahab@cgps.group>
2022-09-26 06:28:04 +0100

    Update react.js

--- react.js
+++ react.js
@@ -3,7 +3,6 @@ module.exports = {
     "plugin:react/recommended",
   ],
   "rules": {
-    "no-unused-vars": [ "error", { "varsIgnorePattern": "PropTypes" }],
     "react/prefer-es6-class": 0,
     "react/prefer-stateless-function": 0,
   },
